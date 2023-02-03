1104f7fe7d7da8ae7f74ff409019316cd6b093fd
Ryan Massey <rmassey.95@gmail.com>
2023-02-03 09:46:29 -0500

    Set default profile image for new users

--- models/user.js
+++ models/user.js
@@ -4,7 +4,12 @@ const { Schema } = mongoose;
 const UserSchema = new Schema({
   username: { type: String, required: true, minLength: 3, trim: true },
   profileImg: { type: String, required: true },
-  password: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+    default:
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
