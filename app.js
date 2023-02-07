a29e41c0d44d7e5ca4f75dd2f48ea8875a0bd067
Ryan Massey <rmassey.95@gmail.com>
2023-02-07 12:43:11 -0500

    Add security to app

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 require("./passport-config");
 
 const express = require("express");
+const helmet = require("helmet");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const rateLimit = require("express-rate-limit");
 
 const taskaidRouter = require("./routes/taskaidRouter");
 
@@ -12,12 +15,26 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
+app.use(limiter);
+app.use(helmet());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      expires: 24 * 60 * 60 * 1000,
+    },
   })
 );
 
