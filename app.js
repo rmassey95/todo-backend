a0ff6b6ab5ddfb01d1de79e56064e46b18b42ef0
Ryan Massey <rmassey.95@gmail.com>
2023-02-11 14:49:29 -0500

    Remove unnecessary code

--- app.js
+++ app.js
@@ -58,18 +58,6 @@ app.use(passport.session());
 // Parse body data from request
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   // allow CORS for React App
-//   res.setHeader("Access-Control-Allow-Origin", process.env.DOMAIN_URL);
-//   // allow crendentials to be sent
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   // allow header to be set in React App
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   // allowed headers in requests
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   next();
-// });
-
 app.use("/taskaid", taskaidRouter);
 
 app.listen(process.env.PORT, () => {
